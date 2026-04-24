6754141f5aa053d277c61bf4f8cc9bf964e46a49
Aymane-Elhassouni <aymane.elhassouni3@gmail.com>
2026-04-24 09:23:29 +0100

    ajouter providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,36 @@ class AppServiceProvider extends ServiceProvider
             \App\Services\Interfaces\AuthServiceInterface::class,
             \App\Services\AuthService::class
         );
+
+        $this->app->bind(
+            \App\DAO\Interfaces\ProfileDAOInterface::class,
+            \App\DAO\Eloquent\EloquentProfileDAO::class
+        );
+
+        $this->app->bind(
+            \App\Services\Interfaces\ProfileServiceInterface::class,
+            \App\Services\ProfileService::class
+        );
+
+        $this->app->bind(
+            \App\Services\Interfaces\TeamServiceInterface::class,
+            \App\Services\TeamService::class
+        );
+
+        $this->app->bind(
+            \App\Services\Interfaces\DashboardServiceInterface::class,
+            \App\Services\DashboardService::class
+        );
+
+        $this->app->bind(
+            \App\Services\Interfaces\TournamentServiceInterface::class,
+            \App\Services\TournamentService::class
+        );
+
+        $this->app->bind(
+            \App\Services\AdminDashboardService::class,
+            \App\Services\AdminDashboardService::class
+        );
     }
 
     /**
